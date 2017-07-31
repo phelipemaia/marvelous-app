e0fe622f276239f380a492341414be231219891e
Phelipe Maia <phelipemaia@gmail.com>
2017-07-31 23:17:25 +0200

    Added ios build

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,3 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-AppRegistry.registerComponent('marvelousapp', () => App);
+AppRegistry.registerComponent('marvelous-app', () => App);
